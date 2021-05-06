2d575c3984367ba7a098bbd9ca3d0648b85a2981
mohdarshad1 <mdarshad181@gmail.com>
2021-05-06 20:39:42 +0530

    [Mohd Arshad] : Add Create Binary Tree

---
+++ lib/src/test/java/com/BinarySearchTree/BinaryTreeTest.java
@@ -0,0 +1,17 @@
+package com.BinarySearchTree;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+	@Test
+	public void given3Numbers_WhenAddedToBST_ShouldReturnSize3() {
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		int size = binaryTree.getSize();
+		assertEquals(3, size);
+	}
+}
