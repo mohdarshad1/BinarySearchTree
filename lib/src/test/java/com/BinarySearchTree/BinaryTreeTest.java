15edfc421b22c3c69132f27f9b98a2db64228ff0
mohdarshad1 <mdarshad181@gmail.com>
2021-05-06 20:53:38 +0530

    [Mohd Arshad] : Search Binary Tree

--- lib/src/test/java/com/BinarySearchTree/BinaryTreeTest.java
+++ lib/src/test/java/com/BinarySearchTree/BinaryTreeTest.java
@@ -1,7 +1,6 @@
 package com.BinarySearchTree;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class BinaryTreeTest {
@@ -14,5 +13,46 @@ public class BinaryTreeTest {
 		binaryTree.add(70);
 		int size = binaryTree.getSize();
 		assertEquals(3, size);
+	}
+	
+	@Test
+	public void createBinary_TreeCheck_UsingSizeMethod() {
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		int size = binaryTree.getSize();
+		//System.out.println("Size is " + size);
+		assertEquals(13, size);
+	}
+	
+	@Test
+	public void givenNumber_WhenSearched_ShouldBeFoundInBST() {
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		int key = binaryTree.search(63);
+		assertEquals(63, key);
 	}
 }
